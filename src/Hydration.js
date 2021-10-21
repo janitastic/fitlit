a64541240f490b4f8a81e6e135556959cfd1fc39
Jani Palomino <palomino.jani@gmail.com>
2021-10-20 19:45:30 -0600

    Add a console log so we can see the average

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,23 +1,26 @@
-class Hydration{
-  constructor(h20Data){
-    this.h20Data = h20Data;
-
-    this.numOunces = numOunces;
+class Hydration {
+  constructor(hydrationData) {
+    this.h20Data = hydrationData;
   }
 
   getAvgOuncesPerDay(id) {
     //1) iteratre throught this.h20 data
     //2) filter through data to get all of it for the given user
-    //2) calculate avg
+    //3) calculate avg
     //4 return result
-    let totalOunces = 0;
-    let userAvgWater = this.h20Data.filter((element) => {
-    element.userID === id
-    totalOunces += element.numOunces
-    })
-    return Math.floor(totalOunces/ this.h20Data.length);
+    let userWaterConsumption = this.h20Data.filter(waterInfo => {
+      return waterInfo.userID === id;
+    });
+
+    let totalOuncesConsumed = userWaterConsumption.reduce((totalOunces, waterInfo) => {
+      return totalOunces + waterInfo.numOunces;
+    }, 0);
+
+    let avgWaterConsumed = Math.round(totalOuncesConsumed / userWaterConsumption.length);
+
+    console.log(`this is our average >>>> ${avgWaterConsumed}`);
+    return avgWaterConsumed;
   }
 }
-
 
 module.exports = Hydration;
