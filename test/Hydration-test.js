ab14c4062ffd0e0becc5a2b16ac027d6bd8ae9b2
Jani Palomino <palomino.jani@gmail.com>
2021-10-21 16:08:45 -0600

    Create calculateWeeklyWater function

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -12,18 +12,27 @@ describe('User', () => {
     let hydrationData = [
       {userID: 1, date: '2019/06/15', numOunces: 37},
       {userID: 1, date: '2019/06/16', numOunces: 42},
-      {userID: 1, date: '2019/06/17', numOunces: 54}
+      {userID: 1, date: '2019/06/17', numOunces: 54},
+      {userID: 1, date: '2019/06/18', numOunces: 65},
+      {userID: 1, date: '2019/06/19', numOunces: 87},
+      {userID: 1, date: '2019/06/20', numOunces: 22},
+      {userID: 1, date: '2019/06/21', numOunces: 16}
     ];
 
     hydrationInfo = new Hydration(hydrationData);
   })
 
   it('should return how much water the user consumed on average', () => {
-    expect(hydrationInfo.getAvgOuncesPerDay(1)).to.equal(44);
+    expect(hydrationInfo.getAvgOuncesPerDay(1)).to.equal(46);
   })
 
   it('should be able to calculate total ounces consumed in a specific day', () => {
     const totalOunces = hydrationInfo.calculateDailyOunces(1, '2019/06/15');
     expect(totalOunces).to.equal(37);
+  })
+
+  it('should return the amount of water consumed each day for a week ', () => {
+    const totalOunces = hydrationInfo.calculateWeeklyWater(1, '2019/06/15');
+    expect(totalOunces).to.deep.equal([37, 42, 54, 65, 87, 22, 16]);
   })
 })
