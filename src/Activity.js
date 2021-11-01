5ca54a26b40b136e0ff794f75f02ad7aeef0a14e
Regan Losey <regan.losey@gmail.com>
2021-11-01 14:58:07 -0700

    Create getDailyMiles function

--- src/Activity.js
+++ src/Activity.js
@@ -1,6 +1,23 @@
+let User = require('../src/User')
+
 class Activity {
-  constructor(activityData) { 
+  constructor(activityData) {
     this.activityData = activityData;
+  }
+
+
+  getDailyMiles(user, selectedDate) {
+    const userActivityInfo = this.activityData.filter(activityInfo => {
+      return activityInfo.userID === User.id;
+    })
+    const dailyMilesWalked = userActivityInfo.find(day => {
+      day.date === selectedDate;
+      
+      let milesWalked = userActivityInfo.numSteps / (5280 / user.strideLength)
+      console.log(milesWalked)
+      return milesWalked
+    })
+    return dailyMilesWalked;
   }
 }
 
