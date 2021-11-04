031cc0ad70364fa46c36501c20e81dc23e993c49
Susanna Carey <susannaopal@gmail.com>
2021-11-04 10:54:40 -0400

    Remove commented out code and fix spacing issues

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,61 +1,7 @@
-// import Chart from 'chart.js/auto';
-// import User from './User';
-// import UserRepository from './UserRepository';
-// import Sleep from './Sleep';
-// import Hydration from './Hydration';
-// import Activity from './Activity';
-// import './css/styles.css';
 import displayCharts from './charts.js';
-// import domUpdates from './domUpdates.js';
-// import './images/add.png';
-// import './images/add-hover.png';
-
-
-// let sleepData;
-// let activityData;
-// let h20Data;
-// let userRepository;
-// let currentUser;
-// let allUsers;
-// let userData;
-// let userh20;
-// let allUsersSleep;
-// const todaysDate = '2020/01/22';
-//
-// import {
-//   fetchUserData,
-//   fetchSleepData,
-//   fetchActivityData,
-//   fetchHydrationData
-// } from './apiCalls.js';
-
-// const fetchAllData = () => {
-//   Promise.all([fetchUserData(), fetchSleepData(), fetchActivityData(), fetchHydrationData()])
-//     .then(allUserData => parseData(allUserData))
-// };
-//
-// const parseData = (allUserData) => {
-//   userData = allUserData[0].userData;
-//   sleepData = allUserData[1].sleepData;
-//   activityData = allUserData[2].activityData;
-//   h20Data = allUserData[3].hydrationData;
-//   allData(userData, sleepData, activityData, h20Data)
-// };
-//
-// const allData = (user, sleep, activity, h20Data) => {
-//   userRepository = new UserRepository(user);
-//   currentUser = new User(userRepository.users[getRandomIndex(userRepository.users)]);
-//   allUsersSleep = new Sleep(sleepData)
-//   userh20 = new Hydration(h20Data);
-//   displayUserInfoCard();
-//   displayCharts(userh20, allUsersSleep, currentUser, todaysDate);
-// };
 
 const displayUserInfoCard = (currentUser) => {
   greeting.innerText = `Welcome, ${currentUser.getFirstName()}!`;
-  // displayUserData();
-  // displayAvgStepGoal();
-  // displaySleepQuality();
 };
 
 const displayUserData = (currentUser) => {
@@ -65,7 +11,6 @@ const displayUserData = (currentUser) => {
   userStepGoal.innerText = `${currentUser.dailyStepGoal}`;
 };
 
-
 const displayAvgStepGoal = (userRepository) => {
   avgStepGoal.innerText = `${userRepository.getAvgStepCount()}`;
 };
@@ -74,8 +19,6 @@ const displaySleepQuality = (allUsersSleep, currentUser, todaysDate) => {
   dailySleepQuality.innerText = allUsersSleep.getDailySleepQual(currentUser.id, todaysDate);
   avgSleepQuality.innerText = allUsersSleep.getAvgDailySleepQual(currentUser.id);
 };
-
-
 
 //QUERY SELECTORS
 const greeting = document.getElementById('firstName');
@@ -107,11 +50,6 @@ logWater.addEventListener('click', showWaterForm);
 logSleep.addEventListener('click', showSleepForm);
 logActivity.addEventListener('click', showActivityForm);
 
-// EVENT LISTENERS
-// window.addEventListener('load', () => {
-//   fetchAllData();
-// });
-
 //HELPER FUNCTIONS
 function hide(element) {
   element.classList.add('hidden')
@@ -125,38 +63,7 @@ function toggle(element) {
   element.classList.toggle('hidden');
 }
 
-// const getRandomIndex = (array) => {
-//   return Math.floor(Math.random() * array.length)
-// };
-
-//DATA DISPLAY FUNCTIONS
-
-// const displayUserInfoCard = () => {
-//   greeting.innerText = `Welcome, ${currentUser.getFirstName()}!`;
-//   displayUserData();
-//   displayAvgStepGoal();
-//   displaySleepQuality();
-// };
-
-// const displayUserData = () => {
-//   userEmail.innerText = `${currentUser.email}`;
-//   userAddress.innerText = `${currentUser.address}`;
-//   userStride.innerText = `${currentUser.strideLength}`;
-//   userStepGoal.innerText = `${currentUser.dailyStepGoal}`;
-// };
-//
-//
-// const displayAvgStepGoal = () => {
-//   avgStepGoal.innerText = `${userRepository.getAvgStepCount()}`;
-// };
-//
-// const displaySleepQuality = () => {
-//   dailySleepQuality.innerText = allUsersSleep.getDailySleepQual(currentUser.id, todaysDate);
-//   avgSleepQuality.innerText = allUsersSleep.getAvgDailySleepQual(currentUser.id);
-// };
-
 //FORM FUNCTIONS
-
 function showWaterForm() {
   toggle(waterChart);
   toggle(waterForm);
@@ -175,5 +82,8 @@ function showActivityForm() {
 
 
 export default domUpdates;
-export {displayUserInfoCard,
-displayUserData, displayAvgStepGoal, displaySleepQuality};
+export {
+displayUserInfoCard,
+displayUserData,
+displayAvgStepGoal,
+displaySleepQuality};
